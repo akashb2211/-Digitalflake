f40a10343dbd63017f59e22cccd0606208b0c126
AKASH BHOSALE <142890463+akashb2211@users.noreply.github.com>
2024-04-27 11:09:25 +0530

    Add files via upload

---
+++ Digitalflek/FRONTEND/product/src/actions/userAction.js
@@ -0,0 +1,93 @@
+// import { USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNOUT, USER_SIGNIN_SUCCESS } from "../constants/userConstants"
+
+// import axios from 'axios';
+
+
+// export const logout=()=>{
+//     return(dispatch)=>{
+//         sessionStorage.removeItem('token')
+//         dispatch({type:USER_SIGNOUT})
+//         document.location.href='/'
+//     }
+// }
+// export const signin = (email_id, password) => {
+//     return (dispatch) => {
+//         dispatch({
+//             type: USER_SIGNIN_REQUEST
+//         })
+//         const header = {
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//         }
+
+//         const body = {
+//             email_id,
+//             password,
+//         }
+//         const url = 'http://localhost:4001/admin/signin'
+//         axios
+//             .post(url, body, header)
+//             .then((response) => {
+//                 dispatch({
+//                     type: USER_SIGNIN_SUCCESS,
+//                     payload: response.data,
+//                 })
+//             })
+//             .catch((error) => {
+//                 dispatch({
+//                     type: USER_SIGNIN_FAIL,
+//                     payload: error,
+//                 })
+//             })
+//     }
+// }
+
+
+
+import { USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNOUT, USER_SIGNIN_SUCCESS } from "../constants/userConstants"
+import axios from 'axios';
+
+export const logout = () => {
+    return (dispatch) => {
+        sessionStorage.removeItem('token');
+        dispatch({ type: USER_SIGNOUT });
+        document.location.href = '/';
+    }
+}
+
+export const signin = (email_id, password) => {
+    return (dispatch) => {
+        dispatch({
+            type: USER_SIGNIN_REQUEST
+        })
+        const header = {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }
+
+        const body = {
+            email_id,
+            password,
+        }
+        const url = 'http://localhost:4001/admin/signin'
+        axios
+            .post(url, body, header)
+            .then((response) => {
+                // Store token in session storage upon successful sign-in
+                sessionStorage.setItem('token', response.data.token);
+                
+                dispatch({
+                    type: USER_SIGNIN_SUCCESS,
+                    payload: response.data,
+                })
+            })
+            .catch((error) => {
+                dispatch({
+                    type: USER_SIGNIN_FAIL,
+                    payload: error,
+                })
+            })
+    }
+}
